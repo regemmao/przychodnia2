5fd4a2f943aa5d3b40c6eecd9af589b52c9c75a9
regemma <regemma@gmail.com>
2013-06-06 20:41:05 +0200

    Revert "znaki nowych linii"
    This reverts commit 963fcc2ec0f553455353881e3a09e711fd7acecc.

--- index2.php
+++ index2.php
@@ -6,20 +6,20 @@ if (!$link) {
 else{
    echo 'Connection OK';
    if(mysqli_select_db($link, 'przychodnia')){
-       echo '<br>wybaï¿½em baze<br>';
+       echo '<br>wyba³em baze<br>';
    }
  else {
      echo 'nie udalo sie';
    }
      $query="INSERT INTO `pracownicy`(`imie`, `nazwisko`, `tytul`) VALUES ('Janusz','Szyszko','lek.')";
-     $query="INSERT INTO pracownicy(imie, nazwisko, tytul) VALUES ('Janusz','Zagï¿½rski','lek.')";
+     $query="INSERT INTO pracownicy(imie, nazwisko, tytul) VALUES ('Janusz','Zagórski','lek.')";
     if($result=mysqli_query($link, $query))
-   {echo "wykonany insert<br>";}
-   else echo "insert si enie pwiodl<br>";
+   {echo "wykonany insert";}
+   else echo "insert si enie pwiodl";
    $query='SELECT * FROM pracownicy';
    if($result=mysqli_query($link, $query))
-   {echo "wykonany select<br>";}
-   else echo "select si enie pwiodl<br>";
+   {echo "wykonany select";}
+   else echo "select si enie pwiodl";
    if($row=  mysqli_fetch_array($result)){
        echo "$row[imie] - $row[nazwisko]<br>";
    }
